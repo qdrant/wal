91bbbf33460e20c41fd5cc9d14aa9a7869c6e1ed
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2024-04-17 16:20:23 +0200

    windows is great?

--- src/lib.rs
+++ src/lib.rs
@@ -953,8 +953,10 @@ mod test {
                 // Create fake temp file to simulate a crash.
                 let mut file = std::fs::OpenOptions::new()
                     .read(true)
+                    .write(true)
                     .create(true)
-                    .append(true)
+                    .append(false)
+                    .truncate(false)
                     .open(dir.path().join("tmp-open-123"))
                     .unwrap();
 
